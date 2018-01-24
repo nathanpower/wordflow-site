c29109526da88fd51c4eb7e6906685577d663f30
Nathan Power <nathan.power@nearform.com>
2018-01-24 23:12:19 +0000

    remove cruft and add normalize.js

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,11 +1,8 @@
 import React from 'react'
 import { getSiteProps } from 'react-static'
-//
-import logoImg from '../logo.png'
 
 export default getSiteProps(() => (
   <div>
-    <h1 style={{ textAlign: 'center' }}>Welcome to</h1>
-    <img src={logoImg} alt="" />
+    <h1 style={{ textAlign: 'center' }}>Welcome to Wordflow</h1>
   </div>
 ))
