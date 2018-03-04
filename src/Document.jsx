f4838057c848e6b3bf972db9b9a2dd1366490c1c
Nathan Power <nathan.power@nearform.com>
2018-03-04 21:16:22 +0000

    start wiring up contact form

--- src/Document.jsx
+++ src/Document.jsx
@@ -5,8 +5,7 @@ export default ({ Html, Head, Body, children }) => (
     <Head>
       <meta charSet="UTF-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" />
-      <link href="https://fonts.googleapis.com/css?family=Poppins" rel="stylesheet" />
+      <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700|Poppins:400,700" rel="stylesheet" />
     </Head>
     <Body>{children}</Body>
   </Html>
--- src/components/contact-form/index.js
+++ src/components/contact-form/index.js
@@ -6,6 +6,33 @@ import PhoneIcon from '../icons/phone'
 import MailIcon from '../icons/mail'
 
 export default class ContactForm extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {}
+  }
+
+  encode (data) {
+    return Object.keys(data)
+      .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(data[key])}`)
+      .join('&')
+  }
+
+  handleChange (e) {
+    this.setState({ [e.target.name]: e.target.value })
+  }
+
+  handleSubmit (e) {
+    fetch('/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: this.encode({ 'form-name': 'contact', ...this.state }),
+    })
+      .then(() => alert('Success!'))
+      .catch(error => alert(error))
+
+    e.preventDefault()
+  }
+
   render () {
     return (
       <div className="contact-form-container row center-xs middle-xs around-xs col-xs-12">
@@ -22,11 +49,22 @@ export default class ContactForm extends React.Component {
             </a>
           </address>
 
-          <form className="row col-xs-12 center-xs between-xs">
-            <input placeholder="Your name *" type="text" className="col-sm-4 col-xs-12" />
-            <input placeholder="Your email *" type="text" className="col-sm-7 col-xs-12" />
-            <input placeholder="Subject" type="text" className="col-xs-12" />
-            <textarea cols="40" rows="12" placeholder="Your message *" type="text" className="col-xs-12" />
+          <form
+            className="contact-form row col-xs-12 center-xs between-xs"
+            name="contact"
+            method="post"
+            data-netlify="true"
+            data-netlify-honeypot="bot-field"
+            onSubmit={this.handleSubmit}
+          >
+            <input hidden name="bot-field" />
+            <input onChange={this.handleChange} placeholder="Your name *" name="name" type="text" className="text-input col-sm-4 col-xs-12" />
+            <input onChange={this.handleChange} placeholder="Your email *" name="email" type="text" className="text-input col-sm-7 col-xs-12" />
+            <input onChange={this.handleChange} placeholder="Subject" name="subject" type="text" className="text-input col-xs-12" />
+            <textarea onChange={this.handleChange} cols="40" rows="12" name="message" placeholder="Your message *" type="text" className="multiline-input col-xs-12" />
+            <div className="btn-container row center-xs middle-xs around-xs col-xs-12">
+              <input value="submit" type="submit" className="contact-submit-btn" />
+            </div>
           </form>
         </div>
       </div>
