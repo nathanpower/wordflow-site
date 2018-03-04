e4e591671dcaaf9504fb836585b671d9a55aca73
Nathan Power <nathan.power@nearform.com>
2018-03-04 13:47:26 +0000

    responsiveness for contact form

--- src/components/contact-form/index.js
+++ src/components/contact-form/index.js
@@ -9,7 +9,7 @@ export default class ContactForm extends React.Component {
   render () {
     return (
       <div className="contact-form-container row center-xs middle-xs around-xs col-xs-12">
-        <div className="contact-form col-xs-7 row center-xs">
+        <div className="contact-form col-md-7 col-sm-10 col-xs-12 row center-xs">
           <h5 className="col-xs-12 phone-cta">Give us a call</h5>
           <address>
             <h2 className="col-xs-12 phone-number">
@@ -23,8 +23,8 @@ export default class ContactForm extends React.Component {
           </address>
 
           <form className="row col-xs-12 center-xs between-xs">
-            <input placeholder="Your name *" type="text" className="col-xs-4" />
-            <input placeholder="Your email *" type="text" className="col-xs-7" />
+            <input placeholder="Your name *" type="text" className="col-sm-4 col-xs-12" />
+            <input placeholder="Your email *" type="text" className="col-sm-7 col-xs-12" />
             <input placeholder="Subject" type="text" className="col-xs-12" />
             <textarea cols="40" rows="12" placeholder="Your message *" type="text" className="col-xs-12" />
           </form>
