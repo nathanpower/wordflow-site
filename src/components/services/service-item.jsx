96d94e037762ce797b2b6dfd76793e457cbbbfb0
Nathan Power <nathan.power@nearform.com>
2018-03-08 00:43:59 +0000

    Revert "prefetchWhenSeen"
    This reverts commit 68a27ea69598a413e827fa2805a95d3507fede3a.

--- src/components/services/service-item.jsx
+++ src/components/services/service-item.jsx
@@ -1,50 +1,48 @@
 
 import React from 'react'
 //
-import { PrefetchWhenSeen, Link } from 'react-static'
+import { Link } from 'react-static'
 
 import './service-item.scss'
 
 export default ({ item }) => (
   <div className="service-item-container col-md-4 col-sm-9 col-xs-12">
-    <PrefetchWhenSeen path={`services/${item.slug}`}>
-      <Link
-        to={`services/${item.slug}`}
-        className="service-item"
+    <Link
+      to={`services/${item.slug}`}
+      className="service-item"
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'flex-end',
+        height: '230px',
+        background: `url('./images/${item.thumbnail}')`,
+        backgroundSize: '100% 100%',
+        backgroundPosition: 'center',
+        margin: '15px',
+        marginBottom: 60,
+        cursor: 'pointer',
+      }}
+    >
+      <div className="overlay" />
+      <div
+        className="service-title"
         style={{
+          height: 50,
+          width: '70%',
+          background: '#159C92',
           display: 'flex',
+          alignItems: 'center',
           justifyContent: 'center',
-          alignItems: 'flex-end',
-          height: '230px',
-          background: `url('./images/${item.thumbnail}')`,
-          backgroundSize: '100% 100%',
-          backgroundPosition: 'center',
-          margin: '15px',
-          marginBottom: 60,
-          cursor: 'pointer',
+          position: 'relative',
+          top: 25,
+          borderRadius: 25,
+          border: '2px solid #159C92',
         }}
       >
-        <div className="overlay" />
-        <div
-          className="service-title"
-          style={{
-            height: 50,
-            width: '70%',
-            background: '#159C92',
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            position: 'relative',
-            top: 25,
-            borderRadius: 25,
-            border: '2px solid #159C92',
-          }}
-        >
-          <h3>
-            {item.title}
-          </h3>
-        </div>
-      </Link>
-    </PrefetchWhenSeen>
+        <h3>
+          {item.title}
+        </h3>
+      </div>
+    </Link>
   </div>
 )
