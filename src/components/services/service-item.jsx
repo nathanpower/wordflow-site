00293bba436b5f835ed34bc226e4533b56508a95
Nathan Power <nathan.power@nearform.com>
2018-03-25 13:04:32 +0100

    auto height for services links on home page

--- src/components/services/service-item.jsx
+++ src/components/services/service-item.jsx
@@ -1,48 +1,64 @@
 
 import React from 'react'
+import debounce from 'lodash.debounce'
 //
 import { Link } from 'react-static'
 
 import './service-item.scss'
 
-export default ({ item }) => (
-  <div className="service-item-container col-md-4 col-sm-9 col-xs-12">
-    <Link
-      to={`/services/${item.slug}`}
-      className="service-item"
-      style={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'flex-end',
-        height: '230px',
-        background: `url('./images/${item.thumbnail}')`,
-        backgroundSize: '100% 100%',
-        backgroundPosition: 'center',
-        margin: '15px',
-        marginBottom: 60,
-        cursor: 'pointer',
-      }}
-    >
-      <div className="overlay" />
-      <div
-        className="service-title"
-        style={{
-          height: 50,
-          width: '70%',
-          background: '#159C92',
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-          position: 'relative',
-          top: 25,
-          borderRadius: 25,
-          border: '2px solid #159C92',
-        }}
-      >
-        <h3>
-          {item.title}
-        </h3>
+export default class ServiceItem extends React.Component {
+  constructor (props) {
+    super(props)
+    this.boundSetRef = this.setRef.bind(this)
+    this.boundSetHeight = debounce(this.setHeight.bind(this), 30, { leading: true })
+    this.state = {
+      height: 230,
+    }
+  }
+
+  componentDidMount () {
+    this.resizeListener = window.addEventListener('resize', this.boundSetHeight)
+  }
+
+  componentWillUnmount () {
+    window.removeEventListener('resize', this.resizeListener)
+  }
+
+  setRef (ref) {
+    this.elRef = ref
+    this.setHeight()
+  }
+
+  setHeight () {
+    if (this.elRef) {
+      const { width } = this.elRef.getBoundingClientRect()
+      this.setState({ height: width * 0.575 })
+    }
+  }
+
+  render () {
+    const { item } = this.props
+    return (
+      <div ref={this.boundSetRef} className="service-item-container col-md-4 col-sm-9 col-xs-12">
+        <Link
+          
+          to={`/services/${item.slug}`}
+          className="service-item"
+          style={{
+            height: this.state.height,
+            background: `url('./images/${item.thumbnail}')`,
+          }}
+        >
+          <div className="overlay" />
+          <div
+            className="service-title"
+          >
+            <h3>
+              {item.title}
+            </h3>
+          </div>
+        </Link>
       </div>
-    </Link>
-  </div>
-)
+    )
+  }
+}
